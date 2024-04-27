707d786829e7df82121a44efe2b760ba7d0d4c11
Mandygando <amandavic7576@gmail.com>
2024-04-26 21:53:25 -0300

    first commit

---
+++ app/controllers/produtos_controller.ts
@@ -0,0 +1,11 @@
+import Produto from "../models/produto.js"
+
+export default class ProdutosController {
+    async index(){
+        return await Produto.all()
+    }
+
+    async store(){
+        return [1,2,3]
+    }
+}
